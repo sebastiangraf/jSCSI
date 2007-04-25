6dcd7469f5fa42b130d1cebaeeeb95a9fd4f2e06
ross_roy <ross_roy@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-04-25 09:05:10 +0000

    Added external ressources required for Whiskas
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@21 fd70404c-346f-40f3-aaf5-4d485f690dad

---
+++ src/org/jscsi/whiskas/preferences/PreferenceInitializer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2007 University of Konstanz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id$
+ * 
+ */
+
+package org.jscsi.whiskas.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.jscsi.whiskas.Activator;
+
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = Activator.getDefault()
+				.getPreferenceStore();
+		store.setDefault(PreferenceConstants.P_INT_HITS_PER_PAINT, "10000");
+		store.setDefault(PreferenceConstants.P_STRING_DEFAULT_LOG_FILE, "");
+		store.setDefault(PreferenceConstants.P_STRING_DEFAULT_TYPE_FILE, "");
+	}
+
+}
