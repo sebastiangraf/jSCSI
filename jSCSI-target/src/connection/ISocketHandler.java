07580ebc1ff6f7b793be0aa9fa06abdfeedea731
marcus_specht <marcus_specht@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-10-02 13:22:54 +0000

    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@97 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- jSCSI-target/src/connection/ISocketHandler.java
+++ jSCSI-target/src/connection/ISocketHandler.java
@@ -10,7 +10,7 @@ import java.net.Socket;
 public interface ISocketHandler {
 	
 	/**
-	 * Assign the PDU to another entity.
+	 * Assign the Socket to another entity.
 	 * @param socket
 	 * @return true if the entity can handle the Socket, false else
 	 */
