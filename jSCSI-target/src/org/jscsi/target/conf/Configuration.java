90dec4252778a375ba275a4f5a7981046b6a591e
marcus_specht <marcus_specht@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-15 12:42:11 +0000

    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@144 fd70404c-346f-40f3-aaf5-4d485f690dad

--- jSCSI-target/src/org/jscsi/target/conf/Configuration.java
+++ jSCSI-target/src/org/jscsi/target/conf/Configuration.java
@@ -1,16 +1,77 @@
 package org.jscsi.target.conf;
 
+import org.jscsi.target.connection.Connection;
+import org.jscsi.target.connection.Session;
+
 public class Configuration {
 
-	public static Configuration create(){
-		return null;
-		
-	}
+	/**
+	 * The relative path (to the project) of the main directory of all
+	 * configuration files.
+	 */
+	public static final String CONFIG_DIR = "conf/";
+
+	/**
+	 * The file name of the XML Schema configuration file for the global
+	 * settings.
+	 */
+	public static final String CONFIGURATION_SCHEMA_FILE = CONFIG_DIR
+			+ "iscsi.xsd";
+
+	/** The file name, which contains all global settings. */
+	public static final String CONFIGURATION_CONFIG_FILE = CONFIG_DIR
+			+ "iscsi.xml";
+
+	public static final String TARGET_PORTAL_GROUP_TAG = "TargetPortalGroupTag";
 	
-	public static final Configuration create(final String configSchemaFileName,
-		      final String configFileName) throws Exception{
-				return null;
-		
+	public static final String TARGET_PORT = "TargetPort";
+	
+	public static final String TARGET_NAME = "TargetName";
+	
+	public static final String TARGET_ADRESS = "TargetAdress";
+	
+
+	public static final Configuration create() {
+		// load configuration file from standard folder
+		return null;
 	}
-		
+
+	public static final Configuration create(final String configSchemaFileName,
+			final String configFileName) throws Exception {
+		// load folder from specified directory
+		return null;
+	}
+
+	public final void addNewConnectionSettings(Connection connection) {
+
+	}
+
+	public final void addNewSessionSettings(Session session) {
+
+	}
+
+	public final void getSessionSettings(Session session) {
+
+	}
+
+	public final void getConnectionSettings(Connection connection) {
+
+	}
+
+	public final void getGlobalSettings() {
+
+	}
+
+	public final void removeConnectionSettings(Connection connection) {
+
+	}
+
+	public final void removeSessionSettings(Session session) {
+
+	}
+
+	private void parseGlobalSettings() {
+
+	}
+
 }
