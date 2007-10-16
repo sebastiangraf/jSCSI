8c1b7b24601a400d50483ba153c7fd6cad8f8923
marcus_specht <marcus_specht@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-16 12:45:26 +0000

    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@156 fd70404c-346f-40f3-aaf5-4d485f690dad

--- jSCSI-target/src/org/jscsi/target/conf/OperationalTextConfiguration.java
+++ jSCSI-target/src/org/jscsi/target/conf/OperationalTextConfiguration.java
@@ -2,8 +2,10 @@ package org.jscsi.target.conf;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,42 +26,21 @@ public class OperationalTextConfiguration {
 	/** The Log interface. */
 	private static final Log LOGGER = LogFactory.getLog(OperationalTextConfiguration.class);
 	
-	/** The ResultFunctionFactory */
-	private final ResultFunctionFactory rfFactory = Singleton.getInstance(ResultFunctionFactory.class);
-	
 	/**
 	 * iSCSI's key Value delimiter
 	 */
 	public static final String KEY_VALUE_DELIMITER = "=";
 
-	/**
-	 * iSCSI's delimiter between multiple values used in value lists
-	 */
-	public static final String VALUE_VALUE_DELIMITER = ",";
+	private final Map<OperationalTextKey, OperationalTextValue> textValuePairs;
 	
-	/** iSCSI's delimiter between two following key-value-pairs*/
-	public static final String PAIR_PAIR_DELIMITER = "\0";
+	public OperationalTextConfiguration(){
+		textValuePairs = new HashMap<OperationalTextKey, OperationalTextValue>();
+	}
 	
-	public static final String SCOPE = "scope";
-	
-	public static final String SCOPE_CONNECTION_WIDE = "connection";
-	
-	public static final String SCOPE_SESSION_WIDE = "session";
-	
-	public static final String SENDER = "sender";
-	
-	public static final String SENDER_TARGET = "target";
-	
-	public static final String SENDER_INITIATOR = "initiator";
-	
-	public static final String SENDER_BOTH = "both";
-	
-	/** String representing the key*/
-	private String key;
-
-	/** String representing the value*/
-	private String value;
-	
+	public static OperationalTextConfiguration createConfiguration(){
+		
+		return null;
+	}
 	
 
 	}
