1a82e613db23431e5b6599efea2c88b6caeef12e
john_quigley <john_quigley@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-18 20:49:06 +0000

    updating interface to remove load() method
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@178 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/tasks/Task.java
+++ projects/scsi/src/org/jscsi/scsi/tasks/Task.java
@@ -11,8 +11,6 @@ public interface Task
 {
    public void execute();
 
-   public void load( TargetTransportPort port, Command command, ByteBuffer output );
-   
    public Command getCommand();
    
    public Nexus getNexus();
