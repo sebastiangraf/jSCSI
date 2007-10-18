7cb22a4dd1c841c12a4f105729d63e69e87c6a36
wleggette <wleggette@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-10-18 20:35:12 +0000

    Removed READ 32 CDB and added exception to TaskFactory.
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@175 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/src/org/jscsi/scsi/tasks/TaskFactory.java
+++ projects/scsi/src/org/jscsi/scsi/tasks/TaskFactory.java
@@ -3,13 +3,15 @@ package org.jscsi.scsi.tasks;
 import java.nio.ByteBuffer;
 
 import org.jscsi.scsi.protocol.Command;
+import org.jscsi.scsi.protocol.sense.exceptions.IllegalRequestException;
 import org.jscsi.scsi.transport.TargetTransportPort;
 
 // TODO: Describe class or interface
 public interface TaskFactory
 {
 
-   Task getInstance( TargetTransportPort port, Command command, ByteBuffer output );
+   Task getInstance( TargetTransportPort port, Command command, ByteBuffer output )
+         throws IllegalRequestException;
    
    
 }
