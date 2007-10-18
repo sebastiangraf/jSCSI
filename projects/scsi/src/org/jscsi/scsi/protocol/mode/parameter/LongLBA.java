4b05bff298dc4199ab4d04b3c6a60ebc026eba09
john_quigley <john_quigley@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-18 20:52:43 +0000

    moving Short/Long LBA parameters into mode page sub-package
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@179 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/protocol/mode/parameter/LongLBA.java
+++ projects/scsi/src/org/jscsi/scsi/protocol/mode/parameter/LongLBA.java
@@ -1,7 +1,9 @@
-package org.jscsi.scsi.protocol.mode;
+package org.jscsi.scsi.protocol.mode.parameter;
 
 import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
+
+import org.jscsi.scsi.protocol.mode.ModePage;
 
 public class LongLBA extends ModePage
 {
