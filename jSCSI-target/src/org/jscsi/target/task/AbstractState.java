94ac20d9feb322f20fa1cbfb682a36f184039124
marcus_specht <marcus_specht@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-10-22 17:34:46 +0000

    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@186 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- jSCSI-target/src/org/jscsi/target/task/AbstractState.java
+++ jSCSI-target/src/org/jscsi/target/task/AbstractState.java
@@ -1,5 +1,30 @@
 package org.jscsi.target.task;
 
-public class AbstractState {
+/**
+ * A State represents one processing instruction set,
+ * that will end the initiator's command request successfully or not,
+ * if an unrecoverable error occurred.  
+ * @author Marcus Specht
+ */
+public abstract class AbstractState extends AbstractOperation implements State, Runnable{
+	
+	
+	/** the State's referenced Task */
+	private final Task refTask;
+	
+	/** Constructor with referenced Task */
+	public AbstractState(Task refTask){
+		this.refTask = refTask;
+	}
+	
+	/**
+	 * Returns the referenced Task
+	 * @return referenced Task
+	 */
+	public Task getReferencedTask() {
+		return refTask;
+	}
+
+
 
 }
