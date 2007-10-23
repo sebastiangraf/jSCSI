4013d106371538fd2d0a3574d6a8015821349285
wleggette <wleggette@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-23 22:39:53 +0000

    Fixed build error in medium error exception.
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@210 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/protocol/sense/exceptions/MediumErrorException.java
+++ projects/scsi/src/org/jscsi/scsi/protocol/sense/exceptions/MediumErrorException.java
@@ -22,56 +22,6 @@ public abstract class MediumErrorException extends SenseException
 
    protected abstract long getLogicalBlockAddress();
    
-   @Override
-   protected byte[] getInformation()
-   {
-      /*
-       * INFORMATION field is populated with LBA for commands which access the medium. This
-       * method should be overwritten by an empty method for those errors not returning an LBA.
-       * Note that addresses less than UINT_MAX will be encoded as 4 bytes, otherwise 8 bytes
-       * will be returned.
-       */
-      
-      ByteArrayOutputStream bs = new ByteArrayOutputStream();
-      DataOutputStream out = new DataOutputStream( bs );
-      
-      long lba = this.getLogicalBlockAddress();
-      
-      if ( lba > 0xFFFFFFFFL )
-      {
-         // greater than UINT_MAX, encode as 8-byte value
-         out.writeLong( lba );
-         out.close();
-      }
-      else if ( lba > 0x7FFFFFFL )
-      {
-         // will be a negative signed int
-         int l = (int)(-(lba - 0x8000000L));
-         out.writeInt(l);
-      }
-      else
-      {
-         out.writeInt((int)lba);
-      }
-      
-      try
-      {
-         out.writeLong(0);
-         out.writeLong(lba);
-         out.close();
-      }
-      catch (IOException e)
-      {
-         throw new RuntimeException("Unable to create serialize exception parameter", e);
-      }
-      
-      this.logicalBlockAddress = bs.toByteArray();
-      assert this.logicalBlockAddress.length == 8 : "Invalid length for error information field";
-      
-      this.actualRetryCount = new ActualRetryCount(actualRetryCount);
-   }
-   
-
 }
 
 
