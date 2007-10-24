e00b44197008d1ae71e720168b12f53ac065a4fd
wleggette <wleggette@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-10-24 23:59:11 +0000

    Finished implementation of GenericTaskManagerTest and fixed some problems with TaskManagerTest.
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@235 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/test/unit/org/jscsi/scsi/tasks/GenericTaskManagerTest.java
+++ projects/scsi/test/unit/org/jscsi/scsi/tasks/GenericTaskManagerTest.java
@@ -9,22 +9,31 @@ import java.util.List;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.jscsi.scsi.exceptions.TaskSetException;
 import org.jscsi.scsi.lu.GenericTaskManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GenericTaskManagerTest extends TaskManagerTest
 {
    
-   @Test
-   public void testStaticInsertion_HSO() throws InterruptedException
+   public static final int MANAGER_THREAD_COUNT = 1; // 1 thread is an arbitrary value
+   
+   
+   @Before
+   public void setUp() throws Exception
    {
       DOMConfigurator.configure(System.getProperty("log4j.configuration"));
-      
-      List<TestTask> taskSet = new ArrayList<TestTask>();
-      new HeadOfQueueTask(taskSet, 0);
-      new SimpleTask(taskSet, 0);
-      new OrderedTask(taskSet, 100);
-      
-      GenericTaskManager manager = new GenericTaskManager(1); // 10 threads is an arbitrary value here
+   }
+
+   @After
+   public void tearDown() throws Exception
+   {
+   }
+
+   
+   private static void executeTaskSet( List<TestTask> taskSet ) throws InterruptedException
+   {
+      GenericTaskManager manager = new GenericTaskManager(MANAGER_THREAD_COUNT);
       
       for ( Task t : taskSet )
       {
@@ -46,12 +55,17 @@ public class GenericTaskManagerTest extends TaskManagerTest
       
       synchronized ( last )
       {
-         last.wait(10000);
+         //last.wait(10000);
+         last.wait();
       }
       
       manager.shutdown();
       thread.join();
       
+   }
+   
+   private static void checkTaskSet( List<TestTask> taskSet )
+   {
       for ( int i = 0; i < taskSet.size(); i++ )
       {
          TestTask t = taskSet.get(i);
@@ -62,8 +76,137 @@ public class GenericTaskManagerTest extends TaskManagerTest
          }
          else
          {
-            assertTrue("Task " + i + " failed (" +t.isProper() + "): " + t.reason() + ": " + t.getClass().getName(), t.isProper() );
-         }  
-      }  
+            assertTrue("Task " + i + " failed (" + t.isProper() + "): " + t.reason() + ": " + t.getClass().getName(), t.isProper() );
+         }
+      }
    }
+   
+   
+   @Test
+   public void testStaticInsertion_HSO() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new HeadOfQueueTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 100);
+      
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+
+   @Test
+   public void testStaticInsertion_HSSO() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new HeadOfQueueTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   @Test
+   public void testStaticInsertion_HOOS() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new HeadOfQueueTask(taskSet, 0);
+      new OrderedTask(taskSet, 0);
+      new OrderedTask(taskSet, 0);
+      new SimpleTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   @Test
+   public void testStaticInsertion_HHSS() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new HeadOfQueueTask(taskSet, 0);
+      new HeadOfQueueTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new SimpleTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   @Test
+   public void testStaticInsertion_SOSSOHH() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 0);
+      new HeadOfQueueTask(taskSet, 0);
+      new HeadOfQueueTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   @Test
+   public void testStaticInsertion_OSOOHH() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new OrderedTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 0);
+      new OrderedTask(taskSet, 0);
+      new HeadOfQueueTask(taskSet, 0);
+      new HeadOfQueueTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   @Test
+   public void testStaticInsertion_SOSSO() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   @Test
+   public void testStaticInsertion_OSSO() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new OrderedTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   @Test
+   public void testStaticInsertion_SSO() throws InterruptedException
+   {
+      List<TestTask> taskSet = new ArrayList<TestTask>();
+      new SimpleTask(taskSet, 0);
+      new SimpleTask(taskSet, 0);
+      new OrderedTask(taskSet, 100);
+      
+      executeTaskSet( taskSet );
+      checkTaskSet( taskSet );
+   }
+   
+   
+   
+   
+
+
 }
