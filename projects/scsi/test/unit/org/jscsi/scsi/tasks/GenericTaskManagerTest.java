d1b254f32b624866779f4d9fc9f8007b0b32beb0
john_quigley <john_quigley@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-10-24 03:53:46 +0000

    refactor and significant cleanup of TaskSet and TaskManager, splitting into interface and "Generic" implementation
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@214 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/test/unit/org/jscsi/scsi/tasks/GenericTaskManagerTest.java
+++ projects/scsi/test/unit/org/jscsi/scsi/tasks/GenericTaskManagerTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.apache.log4j.xml.DOMConfigurator;
 import org.jscsi.scsi.exceptions.TaskSetException;
+import org.jscsi.scsi.lu.GenericTaskManager;
 import org.junit.Test;
 
 // TODO: Describe class or interface
@@ -23,7 +24,7 @@ public class GenericTaskManagerTest extends TaskManagerTest
       new SimpleTask(taskSet, 0);
       Task last = new OrderedTask(taskSet, 100);
       
-      TaskManager manager = new TaskManager(1);  // TODO: Should we test with more threads?
+      GenericTaskManager manager = new GenericTaskManager(1);  // TODO: Should we test with more threads?
       
       for ( Task t : taskSet )
       {
