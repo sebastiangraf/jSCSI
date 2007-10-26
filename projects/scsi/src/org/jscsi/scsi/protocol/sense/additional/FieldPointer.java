a3a7add5a3dad34fd5013e20cff5311afa68cacb
jasonresch <jasonresch@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-26 23:18:35 +0000

    TK-03366
    Removed unused packages
    
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@263 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/protocol/sense/additional/FieldPointer.java
+++ projects/scsi/src/org/jscsi/scsi/protocol/sense/additional/FieldPointer.java
@@ -1,14 +1,8 @@
 
 package org.jscsi.scsi.protocol.sense.additional;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.nio.BufferOverflowException;
-import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
-
-import org.jscsi.scsi.protocol.Encodable;
 
 // TODO: Describe class or interface
 public class FieldPointer implements SenseKeySpecificField
