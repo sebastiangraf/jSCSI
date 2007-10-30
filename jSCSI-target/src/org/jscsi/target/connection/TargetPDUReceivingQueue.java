7dd00163dd8a42388355055e19ba66a2b3701603
marcus_specht <marcus_specht@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-30 14:07:51 +0000

    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@276 fd70404c-346f-40f3-aaf5-4d485f690dad

---
+++ jSCSI-target/src/org/jscsi/target/connection/TargetPDUReceivingQueue.java
@@ -0,0 +1,23 @@
+package org.jscsi.target.connection;
+
+public class TargetPDUReceivingQueue {
+	
+	private final Queue<ProtocolDataUnit> receivingQueue;
+	
+	private final Session refSession;
+	
+	public final ExpectedCommandSequenceNumber;
+	
+	private final int MAX_ALLOWED_SN_GAP = 100;
+	
+	public TargetPDUReceivingQueue(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
