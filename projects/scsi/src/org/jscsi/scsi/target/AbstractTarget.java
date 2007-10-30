bc660875ea226eecaaac7493aa6840db138aa7c7
john_quigley <john_quigley@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-10-30 00:10:57 +0000

    incremental commit of GeneralTarget implementation
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@272 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/src/org/jscsi/scsi/target/AbstractTarget.java
+++ projects/scsi/src/org/jscsi/scsi/target/AbstractTarget.java
@@ -1,20 +1,16 @@
 package org.jscsi.scsi.target;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.jscsi.scsi.authentication.AuthenticationHandler;
 import org.jscsi.scsi.protocol.Command;
-import org.jscsi.scsi.protocol.inquiry.InquiryDataRegistry;
-import org.jscsi.scsi.protocol.mode.ModePageRegistry;
-import org.jscsi.scsi.tasks.TaskFactory;
 import org.jscsi.scsi.tasks.TaskRouter;
 import org.jscsi.scsi.transport.TargetTransportPort;
 
-public class GeneralTarget implements Target
+public abstract class AbstractTarget implements Target
 {
-   private static Logger _logger = Logger.getLogger(GeneralTarget.class);
+   private static Logger _logger = Logger.getLogger(AbstractTarget.class);
    
    
    ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -25,29 +21,6 @@ public class GeneralTarget implements Target
    private TaskRouter _taskRouter;
    private List<AuthenticationHandler> _authHandlers;
 
-   
-   ////////////////////////////////////////////////////////////////////////////////////////////////
-   // constructor(s)
-
-
-   /**
-    * Constructs a new target with a given name, task router, and set of authentication handlers.
-    * 
-    * @param _targetName A target name compatible with the underlying transport layer. For iSCSI
-    *    this must be an IQN or EUI.
-    * @param router The task router. All logical units are registered with the task router.
-    * @param handlers A set of zero or more authentication handlers. At the start of the iSCSI
-    *    login phase the target-available authentication methods are sent to the initiator in
-    *    the order returned by this handler list. Callers must ensure list uniqueness.
-    */
-   public GeneralTarget(String targetName, TaskRouter router, List<AuthenticationHandler> handlers)
-   {
-      this._targetName = targetName;
-      this._taskRouter = router;
-      this._authHandlers = new ArrayList<AuthenticationHandler>();
-      this._authHandlers.addAll(handlers);
-   }
-   
    
    ////////////////////////////////////////////////////////////////////////////////////////////////
    // primary methods
