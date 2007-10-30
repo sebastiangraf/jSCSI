32b4faa02c80f41375d3cdd33a86b427f2a3598d
john_quigley <john_quigley@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-10-30 18:50:23 +0000

    adding basic FileTarget implementation
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@277 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/lu/GeneralLogicalUnit.java
+++ projects/scsi/src/org/jscsi/scsi/lu/GeneralLogicalUnit.java
@@ -1,15 +1,27 @@
 package org.jscsi.scsi.lu;
 
 import org.jscsi.scsi.protocol.Command;
+import org.jscsi.scsi.protocol.mode.ModePageRegistry;
 import org.jscsi.scsi.transport.TargetTransportPort;
 
 // TODO: Describe class or interface
-public abstract class GeneralLogicalUnit implements LogicalUnit
+public class GeneralLogicalUnit implements LogicalUnit
 {
+   public GeneralLogicalUnit()
+   {
+      
+   }
+   
    public void enqueue(TargetTransportPort port, Command command)
    {
       // TODO Auto-generated method stub
 
+   }
+
+   public void setModePageRegistry(ModePageRegistry modePageRegistry)
+   {
+      // TODO Auto-generated method stub
+      
    }
 }
 
