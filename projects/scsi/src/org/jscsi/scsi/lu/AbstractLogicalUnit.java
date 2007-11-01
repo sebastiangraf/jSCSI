64a9e234d3d04e879b6f0c40f8017b50d14f0085
mmotwani <mmotwani@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-11-01 23:38:42 +0000

    Added FileLogicalUnit stub
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@281 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

---
+++ projects/scsi/src/org/jscsi/scsi/lu/AbstractLogicalUnit.java
@@ -0,0 +1,77 @@
+//
+// Cleversafe open-source code header - Version 1.1 - December 1, 2006
+//
+// Cleversafe Dispersed Storage(TM) is software for secure, private and
+// reliable storage of the world's data using information dispersal.
+//
+// Copyright (C) 2005-2007 Cleversafe, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+// USA.
+//
+// Contact Information: Cleversafe, 10 W. 35th Street, 16th Floor #84,
+// Chicago IL 60616
+// email licensing@cleversafe.org
+//
+// END-OF-HEADER
+//-----------------------
+// @author: mmotwani
+//
+// Date: Nov 1, 2007
+//---------------------
+
+package org.jscsi.scsi.lu;
+import org.apache.log4j.Logger;
+import org.jscsi.scsi.protocol.mode.ModePageRegistry;
+import org.jscsi.scsi.tasks.TaskFactory;
+import org.jscsi.scsi.tasks.management.TaskManager;
+
+// TODO: Describe class or interface
+public abstract class AbstractLogicalUnit implements LogicalUnit
+{
+   private static Logger _logger = Logger.getLogger(AbstractLogicalUnit.class);
+
+   private TaskFactory taskFactory;
+   private TaskManager taskManager;
+   
+   public TaskFactory getTaskFactory()
+   {
+      return this.taskFactory;
+   }
+   
+   public void setTaskFactory(TaskFactory taskFactory)
+   {
+      this.taskFactory = taskFactory;
+   }
+   
+   public TaskManager getTaskManager()
+   {
+      return this.taskManager;
+   }
+   
+   public void setTaskManager(TaskManager taskManager)
+   {
+      this.taskManager = taskManager;
+   }
+   
+
+   public void setModePageRegistry(ModePageRegistry modePageRegistry)
+   {
+      // TODO Auto-generated method stub
+
+   }
+}
+
+
