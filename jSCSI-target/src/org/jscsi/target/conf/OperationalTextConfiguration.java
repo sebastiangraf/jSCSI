2538a74a212164dce593b74df4ca15d5b255d925
marcus_specht <marcus_specht@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-01 20:22:14 +0000

    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@282 fd70404c-346f-40f3-aaf5-4d485f690dad

--- jSCSI-target/src/org/jscsi/target/conf/OperationalTextConfiguration.java
+++ jSCSI-target/src/org/jscsi/target/conf/OperationalTextConfiguration.java
@@ -1,15 +1,33 @@
 package org.jscsi.target.conf;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
 import javax.naming.ConfigurationException;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jscsi.target.conf.OperationalTextKey;
 import org.jscsi.target.connection.Connection;
 import org.jscsi.target.connection.Session;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * The OperationalTextConfiguration represents a standard system to work with
@@ -58,24 +76,49 @@ public class OperationalTextConfiguration {
 	private static GlobalConfigParser globalParser;
 
 	private static OperationalTextConfiguration globalConfig;
-	
-	private OperationalTextConfiguration parentConfiguration;
-	
+
+	private final OperationalTextConfiguration parentConfiguration;
+
 	private final Set<OperationalTextKey> localConfig;
 
 	private final String configType;
 
 	private OperationalTextConfiguration(String configType,
-			OperationalTextConfiguration parentConfig) {
+			OperationalTextConfiguration parentConfig)
+			throws OperationalTextException {
 		if (globalParser == null) {
 			globalParser = new GlobalConfigParser(XML_CONF_FILE_ADRESS,
 					XSD_VALIDATE_FILE_ADRESS);
 		}
 		if (globalConfig == null) {
-			globalConfig = parseGlobalConfig();
+			try {
+				globalConfig = parseGlobalConfig();
+			} catch (Exception e) {
+				throw new OperationalTextException(
+						"Couldn't parse global Config: " + e.getMessage());
+			}
 		}
 		this.configType = configType;
+		parentConfiguration = parentConfig;
+		localConfig = new HashSet<OperationalTextKey>();
+	}
 
+	/**
+	 * Constructor for global Configuration
+	 * 
+	 * @param configType
+	 *            "CONFIG_TYPE_GLOBAL"
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 */
+	private OperationalTextConfiguration() {
+		if (globalParser == null) {
+			globalParser = new GlobalConfigParser(XML_CONF_FILE_ADRESS,
+					XSD_VALIDATE_FILE_ADRESS);
+		}
+		this.configType = CONFIG_TYPE_GLOBAL;
+		parentConfiguration = null;
 		localConfig = new HashSet<OperationalTextKey>();
 	}
 
@@ -90,7 +133,6 @@ public class OperationalTextConfiguration {
 			throw new OperationalTextException(
 					"Confiduration already contains key: " + key.getKey());
 		}
-
 	}
 
 	/**
@@ -108,8 +150,58 @@ public class OperationalTextConfiguration {
 				return testedKey;
 			}
 		}
+		if (parentConfiguration.configType.equals(CONFIG_TYPE_GLOBAL)) {
+			return parentConfiguration.getKey(key);
+		}
 		throw new OperationalTextException(
 				"Configuration doesn't contain Key: " + key);
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 * @throws ConfigurationException
+	 */
+	public Set<OperationalTextKey> getSenderKeys(String targetOrInitiator)
+			throws OperationalTextException {
+		Set<OperationalTextKey> result = new HashSet<OperationalTextKey>();
+		Iterator<OperationalTextKey> keys = getConfigSet().iterator();
+		while (keys.hasNext()) {
+			OperationalTextKey testedKey = keys.next();
+			if (testedKey.getSender().equals(targetOrInitiator)) {
+				result.add(testedKey);
+			}
+		}
+		if (!parentConfiguration.configType.equals(CONFIG_TYPE_GLOBAL)) {
+			return getSenderKeys(targetOrInitiator, result);
+		}
+		throw new OperationalTextException(
+				"Configuration doesn't contain keys with sender: "
+						+ targetOrInitiator);
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 * @throws ConfigurationException
+	 */
+	private Set<OperationalTextKey> getSenderKeys(String targetOrInitiator,
+			Set<OperationalTextKey> result) throws OperationalTextException {
+		Iterator<OperationalTextKey> keys = getConfigSet().iterator();
+		while (keys.hasNext()) {
+			OperationalTextKey testedKey = keys.next();
+			if (testedKey.getSender().equals(targetOrInitiator)) {
+				result.add(testedKey);
+			}
+		}
+		if (!parentConfiguration.configType.equals(CONFIG_TYPE_GLOBAL)) {
+			return getSenderKeys(targetOrInitiator, result);
+		}
+		throw new OperationalTextException(
+				"Configuration doesn't contain keys with sender: "
+						+ targetOrInitiator);
 	}
 
 	/**
@@ -140,29 +232,42 @@ public class OperationalTextConfiguration {
 		}
 	}
 
-	public void reset(String key) {
-
-	}
-
-	public void delete() {
-
-	}
-
+	/**
+	 * Check if two configurations are equal.
+	 * 
+	 * @param config
+	 * @return true if equals, false else.
+	 */
 	public boolean equals(OperationalTextConfiguration config) {
-		if (this.getConfigSet().containsAll(config.getConfigSet())) {
+		if (config == null) {
+			return false;
+		}
+		if (this.getConfigSet().containsAll(config.getConfigSet())
+				&& (this.getConfigSet().size() == config.getConfigSet().size())) {
 			return true;
 		}
 		return false;
 	}
 
-	public static OperationalTextConfiguration create(Session session) {
+	/**
+	 * Creates an OperationalTextConfiguration for an iSCSI Session Object.
+	 * Configuration will be created holding all parameters with "scope=sessio".
+	 * 
+	 * @param session
+	 *            The Configurations Session
+	 * @return
+	 * @throws OperationalTextException
+	 */
+	public static OperationalTextConfiguration create(Session session)
+			throws OperationalTextException {
 		OperationalTextConfiguration result = new OperationalTextConfiguration(
 				OperationalTextConfiguration.CONFIG_TYPE_SESSION, globalConfig);
 		result.reset();
 		return result;
 	}
 
-	public static OperationalTextConfiguration create(Connection connection) {
+	public static OperationalTextConfiguration create(Connection connection)
+			throws OperationalTextException {
 		OperationalTextConfiguration result = new OperationalTextConfiguration(
 				OperationalTextConfiguration.CONFIG_TYPE_CONNECTION, connection
 						.getReferencedSession().getConfiguration());
@@ -170,14 +275,16 @@ public class OperationalTextConfiguration {
 		return result;
 	}
 
-	public static OperationalTextConfiguration getGlobalConfig() {
-		if (globalConfig == null) {
-			globalConfig = parseGlobalConfig();
-		}
+	private static OperationalTextConfiguration createEmptyGlobalConfig() {
+		return new OperationalTextConfiguration();
+	}
+
+	protected static OperationalTextConfiguration getGlobalConfig() {
 		return globalConfig;
 	}
 
-	public static OperationalTextConfiguration parseGlobalConfig() {
+	public static OperationalTextConfiguration parseGlobalConfig()
+			throws SAXException, ParserConfigurationException, IOException {
 		return globalParser.parse();
 	}
 
@@ -189,19 +296,35 @@ public class OperationalTextConfiguration {
 		return result.toString();
 	}
 
-	public static String toString(OperationalTextConfiguration config) {
+	public static String toString(Set<OperationalTextKey> keySet) {
 		StringBuffer result = new StringBuffer();
-		Iterator<OperationalTextKey> pairs = config.getConfigSet().iterator();
-		while (pairs.hasNext()) {
-			result.append(toString(pairs.next().getKey(), pairs.next()
-					.getValue().getString()));
+		Iterator<OperationalTextKey> keys = keySet.iterator();
+		while (keys.hasNext()) {
+			result.append(toString(keys.next().getKey(), keys.next().getValue()
+					.getString()));
 			result.append(PAIR_DELIMITER);
 		}
 		result.deleteCharAt(result.length() - 1);
 		return result.toString();
 	}
 
+	public static String toString(OperationalTextConfiguration config) {
+		return toString(config.getConfigSet());
+	}
+
 	private class GlobalConfigParser {
+
+		/** The XML element name of the global node. */
+		private static final String ELEMENT_GLOBAL = "global";
+
+		/** The name of the XML attribute of the result function of this key. */
+		private static final String ATTRIBUTE_RESULT = "result";
+
+		/** The name of the XML attribute of the scope of this key. */
+		private static final String ATTRIBUTE_SCOPE = "scope";
+
+		/** The name of the XML attribute of the sender of this key. */
+		private static final String ATTRIBUTE_SENDER = "sender";
 
 		private final String XML_FILE_ADRESS;
 		private final String XSD_FILE_ADRESS;
@@ -211,9 +334,138 @@ public class OperationalTextConfiguration {
 			XSD_FILE_ADRESS = xsdFileAdress;
 		}
 
-		public synchronized OperationalTextConfiguration parse() {
-			// don't forget to mark created globalConfig as GlOBAL_WIDE;
-			return null;
+		public synchronized OperationalTextConfiguration parse()
+				throws SAXException, ParserConfigurationException, IOException {
+			Element root = (Element) parseXMLDocument(XSD_FILE_ADRESS,
+					XML_FILE_ADRESS);
+			if (root == null) {
+				throw new NullPointerException();
+			}
+			return parseGlobalKeys(root);
+		}
+
+		/**
+		 * Reads the given configuration file in memory and creates a DOM
+		 * representation.
+		 * 
+		 * @throws SAXException
+		 *             If this operation is supported but failed for some
+		 *             reason.
+		 * @throws ParserConfigurationException
+		 *             If a <code>DocumentBuilder</code> cannot be created
+		 *             which satisfies the configuration requested.
+		 * @throws IOException
+		 *             If any IO errors occur.
+		 */
+		private final Document parseXMLDocument(final String schemaFile,
+				final String configFile) throws SAXException,
+				ParserConfigurationException, IOException {
+
+			final SchemaFactory schemaFactory = SchemaFactory
+					.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+			final File schemaLocation = new File(schemaFile);
+			final Schema schema = schemaFactory.newSchema(schemaLocation);
+
+			// create a validator for the document
+			final Validator validator = schema.newValidator();
+
+			final DocumentBuilderFactory domFactory = DocumentBuilderFactory
+					.newInstance();
+			domFactory.setNamespaceAware(true); // never forget this
+			final DocumentBuilder builder = domFactory.newDocumentBuilder();
+			final Document doc = builder.parse(new File(configFile));
+
+			final DOMSource source = new DOMSource(doc);
+			final DOMResult result = new DOMResult();
+
+			validator.validate(source, result);
+			return (Document) result.getNode();
+		}
+
+		/**
+		 * Parses all global settings form the main configuration file.
+		 * 
+		 * @param root
+		 *            The root element of the configuration.
+		 */
+		private final OperationalTextConfiguration parseGlobalKeys(
+				final Element root) {
+			// new configuration
+			OperationalTextConfiguration newGlobalConfig = OperationalTextConfiguration
+					.createEmptyGlobalConfig();
+			// parse every key from xml to globalConfig
+			final NodeList globalConfiguration = root
+					.getElementsByTagName(ELEMENT_GLOBAL);
+			Node parameter;
+			NodeList parameters;
+			NamedNodeMap attributes;
+			OperationalTextKey key = null;
+			OperationalTextValue value = null;
+			String newKey;
+			String scope;
+			String sender = null;
+			String resultType;
+			String newValue;
+			for (int i = 0; i < globalConfiguration.getLength(); i++) {
+				parameters = globalConfiguration.item(i).getChildNodes();
+
+				for (int j = 0; j < parameters.getLength(); j++) {
+					parameter = parameters.item(j);
+					newKey = parameter.getNodeName();
+					if (parameter.getNodeType() == Node.ELEMENT_NODE) {
+						attributes = parameter.getAttributes();
+						scope = attributes.getNamedItem(ATTRIBUTE_SCOPE)
+								.getNodeValue();
+						resultType = attributes.getNamedItem(ATTRIBUTE_RESULT)
+								.getNodeValue();
+						sender = attributes.getNamedItem(ATTRIBUTE_SENDER)
+								.getNodeValue();
+						// key.setSender(attributes.getNamedItem(ATTRIBUTE_SENDER).getNodeValue());
+						newValue = parameter.getTextContent();
+						try {
+							key = OperationalTextKey.create(newKey, scope,
+									sender);
+						} catch (OperationalTextException e) {
+							if (LOGGER.isDebugEnabled()) {
+								// this will never happen, but an empty catch
+								// block...never :)
+								LOGGER
+										.debug("Couldn't parse key from iscsi.xsd: "
+												+ e.getMessage());
+							}
+						}
+						try {
+							value = OperationalTextValue.create(newValue,
+									resultType);
+						} catch (OperationalTextException e) {
+							if (LOGGER.isDebugEnabled()) {
+								// this will never happen, but an empty catch
+								// block...never :)
+								LOGGER
+										.debug("Couldn't parse key from iscsi.xml: "
+												+ e.getMessage());
+							}
+						}
+						key.setValue(value);
+
+						synchronized (globalConfig) {
+							try {
+								newGlobalConfig.addKey(key);
+							} catch (OperationalTextException e) {
+								if (LOGGER.isDebugEnabled()) {
+									// this will never happen, but an empty
+									// catch block...never :)
+									LOGGER
+											.debug("iscsi.xml contains key twice: "
+													+ e.getMessage());
+								}
+								;
+							}
+						}
+					}
+				}
+			}
+			return newGlobalConfig;
 		}
 
 	}
