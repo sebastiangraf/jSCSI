6c5e78f8286c6009bd1ae5de2bbfd0d3237861a3
mmotwani <mmotwani@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-01 06:28:08 +0000

    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@281 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/target/DefaultTarget.java
+++ projects/scsi/src/org/jscsi/scsi/target/DefaultTarget.java
@@ -1,6 +1,8 @@
+
 package org.jscsi.scsi.target;
 
 import org.jscsi.scsi.lu.GeneralLogicalUnit;
+import org.jscsi.scsi.lu.LogicalUnit;
 import org.jscsi.scsi.protocol.Command;
 import org.jscsi.scsi.protocol.inquiry.StaticInquiryDataRegistry;
 import org.jscsi.scsi.protocol.mode.StaticModePageRegistry;
@@ -11,14 +13,14 @@ import org.jscsi.scsi.transport.TargetTransportPort;
 public class DefaultTarget extends AbstractTarget
 {
    private TaskRouter _taskRouter;
-   
+
    public DefaultTarget(String targetName) throws Exception
    {
       this.setTargetName(targetName);
 
-      this._taskRouter = new GeneralTaskRouter(new TargetTaskFactory(),
-                                               new StaticModePageRegistry(),
-                                               new StaticInquiryDataRegistry());
+      this._taskRouter =
+            new GeneralTaskRouter(new TargetTaskFactory(), new StaticModePageRegistry(),
+                  new StaticInquiryDataRegistry());
       this._taskRouter.registerLogicalUnit(0, new GeneralLogicalUnit());
    }
 
@@ -26,5 +28,24 @@ public class DefaultTarget extends AbstractTarget
    public void enqueue(TargetTransportPort port, Command command)
    {
       this._taskRouter.enqueue(port, command);
+   }
+
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.jscsi.scsi.target.Target#nexusLost()
+    */
+   public void nexusLost()
+   {
+   }
+
+   public void registerLogicalUnit(long id, LogicalUnit lu) throws Exception
+   {
+      this._taskRouter.registerLogicalUnit(id, lu);
+   }
+
+   public void removeLogicalUnit(long id) throws Exception
+   {
+      this._taskRouter.removeLogicalUnit(id);
    }
 }
--- projects/scsi/src/org/jscsi/scsi/transport/TargetTransportPort.java
+++ projects/scsi/src/org/jscsi/scsi/transport/TargetTransportPort.java
@@ -1,3 +1,4 @@
+
 package org.jscsi.scsi.transport;
 
 import java.nio.ByteBuffer;
@@ -11,44 +12,71 @@ import org.jscsi.scsi.tasks.Status;
  * <p>
  * The target port is responsible for enqueuing incoming commands onto the Task Router of the
  * appropriate SCSI target. As the Task Router does not distinguish between command and data
- * transfer services the transport port is responsible for writing all expected incoming data
- * to a byte buffer and enqueuing that buffer along with the incoming command. The transport port
- * must then process returning status, sense data, and input data buffers which are sent directly
- * from Logical Units.
+ * transfer services the transport port is responsible for writing all expected incoming data to a
+ * byte buffer and enqueuing that buffer along with the incoming command. The transport port must
+ * then process returning status, sense data, and input data buffers which are sent directly from
+ * Logical Units.
  */
 public interface TargetTransportPort
 {
-   
+
    /**
     * Registers a SCSI Target. The target name is used by the transport services to advertise the
     * target. Incoming commands are enqueued onto the target's Task Router.
     * 
-    * @param target The target to register.
+    * @param target
+    *           The target to register.
     */
    void registerTarget(Target target);
-   
+
    /**
-    * Removes a SCSI Target. After removal no further commands will be sent to the target. 
-    * @param targetName The name of the target to remove.
-    * @throws Exception If a target with the given name has not be registered.
+    * Removes a SCSI Target. After removal no further commands will be sent to the target.
+    * 
+    * @param targetName
+    *           The name of the target to remove.
+    * @throws Exception
+    *            If a target with the given name has not be registered.
     */
    void removeTarget(String targetName) throws Exception;
-   
-   boolean readData( Nexus nexus, ByteBuffer output );
 
-   boolean writeData( Nexus nexus, ByteBuffer input );
-
-   void terminateDataTransfer( Nexus nexus );
-   
    /**
-    * Enqueues return data to send to the initiator indicated by the given Nexus. Used by both
-    * Task Routers and Logical Units, depending on the original command.
+    * Called from the Task implementation for Write commands among others. TODO Finish
+    * documentation.
     * 
-    * @param nexus The Nexus of the original SCSI request. 
-    * @param input The input buffer if return data is expected; <code>null</code> if no return
-    *    data expected or a negative condition precludes returning data.
-    * @param status The command status.
-    * @param senseData Autosense data; <code>null</code> if a positive status was returned.
+    * @param nexus
+    * @param commandReferenceNumber
+    * @param output
+    * @return
     */
-   void writeResponse( Nexus nexus, Status status, ByteBuffer senseData );   
+   boolean readData(Nexus nexus, long commandReferenceNumber, ByteBuffer output);
+
+   /**
+    * Called from the Task implementation for Read commands among others. TODO Finish documentation.
+    * 
+    * @param nexus
+    * @param commandReferenceNumber
+    * @param input
+    * @return
+    */
+   boolean writeData(Nexus nexus, long commandReferenceNumber, ByteBuffer input);
+
+   void terminateDataTransfer(Nexus nexus);
+
+   /**
+    * Enqueues return data to send to the initiator indicated by the given Nexus. Used by both Task
+    * Routers and Logical Units, depending on the original command.
+    * 
+    * @param nexus
+    *           The Nexus of the original SCSI request.
+    * @param commandReferenceNumber
+    *           TODO
+    * @param status
+    *           The command status.
+    * @param senseData
+    *           Autosense data; <code>null</code> if a positive status was returned.
+    * @param input
+    *           The input buffer if return data is expected; <code>null</code> if no return data
+    *           expected or a negative condition precludes returning data.
+    */
+   void writeResponse(Nexus nexus, long commandReferenceNumber, Status status, ByteBuffer senseData);
 }
