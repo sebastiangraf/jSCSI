1533fe1975f0d617f7262f97a91bfbef2828a21e
john_quigley <john_quigley@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-09 08:02:45 +0000

    further cleanup, all tests now build, though logic needs to be checked
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@321 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/tasks/management/DefaultTaskSet.java
+++ projects/scsi/src/org/jscsi/scsi/tasks/management/DefaultTaskSet.java
@@ -1,37 +1,3 @@
-//
-// Cleversafe open-source code header - Version 1.1 - December 1, 2006
-//
-// Cleversafe Dispersed Storage(TM) is software for secure, private and
-// reliable storage of the world's data using information dispersal.
-//
-// Copyright (C) 2005-2007 Cleversafe, Inc.
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-// USA.
-//
-// Contact Information: Cleversafe, 10 W. 35th Street, 16th Floor #84,
-// Chicago IL 60616
-// email licensing@cleversafe.org
-//
-// END-OF-HEADER
-//-----------------------
-// Author: wleggette
-//
-// Date: Nov 5, 2007
-//---------------------
-
 package org.jscsi.scsi.tasks.management;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -43,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -51,7 +16,6 @@ import java.util.concurrent.locks.ReentrantLock;
 import org.apache.log4j.Logger;
 import org.jscsi.scsi.protocol.Command;
 import org.jscsi.scsi.protocol.sense.exceptions.OverlappedCommandsAttemptedException;
-import org.jscsi.scsi.target.Target;
 import org.jscsi.scsi.tasks.Status;
 import org.jscsi.scsi.tasks.Task;
 import org.jscsi.scsi.tasks.TaskAttribute;
