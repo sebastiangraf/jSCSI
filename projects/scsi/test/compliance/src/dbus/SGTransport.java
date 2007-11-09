352a80baf2e19b6146e5823ca052590426224c13
mmotwani <mmotwani@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-11-09 23:23:44 +0000

    Moved SG interfaces to test/compliance; Added DBUS java libraries; updated classpath.
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@332 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

---
+++ projects/scsi/test/compliance/src/dbus/SGTransport.java
@@ -0,0 +1,26 @@
+
+package src.dbus;
+
+import org.freedesktop.dbus.DBusInterface;
+
+public interface SGTransport extends DBusInterface
+{
+   /**
+    * service_indication(Nexus, CDB, Task Attribute, Auto Sense Request, Command Reference Number)
+    * 
+    * Note: 1. we are foregoing the explicit use of "Auto Sense Request" 2. TaskAttribute is always
+    * simple, as SG3 supports nothing else 3. TaskTag is generated from the TransportPortLayer;
+    * generally it will be the same as the cmdRef
+    * 
+    * @param initiatorPort
+    *           Part of the Nexus
+    * @param targetPort
+    *           Part of the Nexus
+    * @param lun
+    *           Part of the Nexus
+    * @param cdb
+    * @param cmdRef
+    */
+   void serviceIndication(String initiatorPort, String targetPort, long lun, byte[] cdb, long cmdRef);
+
+}
