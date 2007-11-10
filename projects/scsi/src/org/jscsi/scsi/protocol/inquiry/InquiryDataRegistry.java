867a276abe3a224f94dae8b3a4fda4db34258cb3
john_quigley <john_quigley@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-11-10 00:52:07 +0000

    incremental commit: implementing VPD pages
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@333 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/src/org/jscsi/scsi/protocol/inquiry/InquiryDataRegistry.java
+++ projects/scsi/src/org/jscsi/scsi/protocol/inquiry/InquiryDataRegistry.java
@@ -1,45 +1,143 @@
-//
-// Cleversafe open-source code header - Version 1.1 - December 1, 2006
-//
-// Cleversafe Dispersed Storage(TM) is software for secure, private and
-// reliable storage of the world's data using information dispersal.
-//
-// Copyright (C) 2005-2007 Cleversafe, Inc.
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-// USA.
-//
-// Contact Information: Cleversafe, 10 W. 35th Street, 16th Floor #84,
-// Chicago IL 60616
-// email licensing@cleversafe.org
-//
-// END-OF-HEADER
-//-----------------------
-// @author: jquigley
-//
-// Date: Oct 22, 2007
-//---------------------
-
 package org.jscsi.scsi.protocol.inquiry;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
-// Contains instances of InquiryData objects
-public abstract class InquiryDataRegistry
+import org.jscsi.scsi.protocol.Serializer;
+import org.jscsi.scsi.protocol.mode.BackgroundControl;
+import org.jscsi.scsi.protocol.mode.Caching;
+import org.jscsi.scsi.protocol.mode.Control;
+import org.jscsi.scsi.protocol.mode.ControlExtension;
+import org.jscsi.scsi.protocol.mode.DisconnectReconnect;
+import org.jscsi.scsi.protocol.mode.InformationalExceptionsControl;
+import org.jscsi.scsi.protocol.mode.ModePage;
+import org.jscsi.scsi.protocol.mode.PowerCondition;
+import org.jscsi.scsi.protocol.mode.ReadWriteErrorRecovery;
+import org.jscsi.scsi.protocol.util.ByteBufferInputStream;
+
+/**
+ * 
+ */
+public abstract class InquiryDataRegistry implements Serializer
 {
-   protected StandardInquiryData inquiryData = null;
+   // Long to ModePage map
+   private Map<Long, ModePage> pages = new HashMap<Long, ModePage>();
+
+   private long getParserID(byte pageCode, int subPageCode)
+   {
+      return (pageCode << 32) | subPageCode;
+   }
+
+   // Factory registration methods
+   private void register(byte pageCode, ModePage page)
+   {
+      register(pageCode, -1, page);
+   }
+
+   private void register(byte pageCode, int subPageCode, ModePage page)
+   {
+      pages.put(getParserID(pageCode, subPageCode), page);
+   }
+
+   public ModePage getModePage(byte pageCode, int subPageCode)
+   {
+      return pages.get(getParserID(pageCode, subPageCode));
+   }
+
+   // Inquiry pages
+
+   protected StandardInquiryData standardInquiryData = null;
    
+   
+   
+   
+   public InquiryDataRegistry()
+   {
+      standardInquiryData = new StandardInquiryData();
+
+      // initialize the pages
+      registerObjects();
+      populateInquiryPages();
+   }
+
+   protected abstract void populateInquiryPages();
+
+   // Registration
+   private void registerObjects()
+   {/*
+      register(StandardInquiryData.PAGE_CODE);
+      register(BackgroundControl.PAGE_CODE, BackgroundControl.SUBPAGE_CODE, backgroundControl);
+      register(Caching.PAGE_CODE, caching);
+      register(Control.PAGE_CODE, control);
+      register(ControlExtension.PAGE_CODE, ControlExtension.SUBPAGE_CODE, controlExtension);
+      register(DisconnectReconnect.PAGE_CODE, disconnectReconnect);
+      register(InformationalExceptionsControl.PAGE_CODE, informationalExceptionsControl);
+      register(PowerCondition.PAGE_CODE, powerCondition);
+      register(ReadWriteErrorRecovery.PAGE_CODE, readWriteErrorRecovery);
+      */
+   }
+
+   @SuppressWarnings("unchecked")
+   public ModePage decode(ByteBuffer buffer) throws IOException
+   {
+      DataInputStream dataIn = new DataInputStream(new ByteBufferInputStream(buffer.duplicate()));
+
+      boolean subPageFormat;
+      byte[] header;
+
+      dataIn.mark(0);
+      int b0 = dataIn.readUnsignedByte();
+      dataIn.reset();
+
+      subPageFormat = ((b0 >>> 6) & 0x01) == 1;
+
+      byte pageCode = (byte) (b0 & 0x3F);
+      int subPageCode;
+
+      if (subPageFormat)
+      {
+         header = new byte[4];
+         dataIn.read(header);
+
+         subPageCode = header[1];
+      }
+      else
+      {
+         header = new byte[2];
+         dataIn.read(header);
+
+         subPageCode = -1;
+      }
+
+      ModePage page = getModePage(pageCode, subPageCode);
+      
+      if(page != null)
+      {
+         page.decode(header, buffer);
+         return page;
+      }
+      else
+      {
+         throw new RuntimeException("Invalid pageCode/subPageCode - (" + pageCode + "/"
+               + subPageCode + ") no corresponding ModePage found");
+
+      }
+   }
+
+   public void saveModePages(ByteBuffer pages) throws BufferUnderflowException, IOException
+   {
+      while (pages.position() < pages.limit())
+      {
+         decode(pages);
+      }
+   }
+
+   public StandardInquiryData getStandardInquiryData()
+   {
+      return this.standardInquiryData;
+   }
 }
-
-
