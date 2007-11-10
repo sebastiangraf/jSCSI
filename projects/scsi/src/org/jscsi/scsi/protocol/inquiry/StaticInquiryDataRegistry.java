963f0d36b3ad408b7629fbe360d349de28117e16
john_quigley <john_quigley@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-10 00:52:07 +0000

    incremental commit: implementing VPD pages
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@335 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/protocol/inquiry/StaticInquiryDataRegistry.java
+++ projects/scsi/src/org/jscsi/scsi/protocol/inquiry/StaticInquiryDataRegistry.java
@@ -1,6 +1,10 @@
 package org.jscsi.scsi.protocol.inquiry;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 import org.apache.log4j.Logger;
+import org.jscsi.scsi.protocol.Encodable;
 
 // TODO: this class must be implemented
 public class StaticInquiryDataRegistry extends InquiryDataRegistry
@@ -9,5 +13,19 @@ public class StaticInquiryDataRegistry extends InquiryDataRegistry
 
    public StaticInquiryDataRegistry()
    {
+   }
+
+   @Override
+   protected void populateInquiryPages()
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public <T extends Encodable> T decode(ByteBuffer buffer) throws IOException
+   {
+      // TODO Auto-generated method stub
+      return null;
    }
 }
