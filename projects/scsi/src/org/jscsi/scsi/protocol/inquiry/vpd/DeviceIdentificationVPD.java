cbc55f730ad37d9c088846b70849ef71b3f17e6e
john_quigley <john_quigley@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-11-13 19:07:40 +0000

    updating SupportedVPDPages implementation
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@357 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/src/org/jscsi/scsi/protocol/inquiry/vpd/DeviceIdentificationVPD.java
+++ projects/scsi/src/org/jscsi/scsi/protocol/inquiry/vpd/DeviceIdentificationVPD.java
@@ -1,37 +1,105 @@
 package org.jscsi.scsi.protocol.inquiry.vpd;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.jscsi.scsi.protocol.util.ByteBufferInputStream;
 
 public class DeviceIdentificationVPD extends VPDPage
 {
    private static Logger _logger = Logger.getLogger(DeviceIdentificationVPD.class);
    
    public static final int PAGE_CODE = 0x83;
+   
+   private List<IdentificationDescriptor> descriptorList = new LinkedList<IdentificationDescriptor>();
 
-   public DeviceIdentificationVPD()
+   public DeviceIdentificationVPD(int peripheralQualifier, int peripheralDeviceType, List<IdentificationDescriptor> descriptorList)
    {
+      this.setPeripheralQualifier(peripheralQualifier);
+      this.setPeripheralDeviceType(peripheralDeviceType);
+      this.descriptorList = descriptorList;
+   }
+   
+   public DeviceIdentificationVPD(int peripheralQualifier, int peripheralDeviceType)
+   {
+      this(peripheralQualifier, peripheralDeviceType, new LinkedList<IdentificationDescriptor>());
    }
    
    @Override
    public void decode(byte[] header, ByteBuffer buffer) throws IOException
    {
-
+      DataInputStream in = new DataInputStream(new ByteBufferInputStream(buffer));
+      
+      // byte 0
+      int b0 = in.readUnsignedByte();
+      this.setPeripheralQualifier(b0 >>> 5);
+      this.setPeripheralDeviceType(b0 & 0x1F);
+      
+      // byte 1
+      int b1 = in.readUnsignedByte();
+      
+      if (b1 != PAGE_CODE)
+      {
+         throw new IOException("invalid page code: " + Integer.toHexString(b1));
+      }
+      
+      this.setPageCode(b1);
+      
+      // byte 2 - 3
+      int pageLength = in.readUnsignedShort();
+      
+      // byte 3
+      int b3 = in.readUnsignedByte();
+      
+      // identification descriptor list
+      descriptorList = IdentificationDescriptor.parse(in);
    }
-
-
 
    @Override
-   public byte[] encode() throws BufferOverflowException
+   public byte[] encode() throws IOException
    {
+      int pageLength = 0;
+      for (IdentificationDescriptor id : descriptorList)
+      {
+         pageLength += id.getIdentifierLength() + 4;
+      }
+      
+      ByteArrayOutputStream baos = new ByteArrayOutputStream(pageLength + 4);
+      DataOutputStream out = new DataOutputStream(baos);
 
-      return null;
+      try
+      {
+         // byte 0
+         out.writeByte((this.getPeripheralQualifier() << 5)|this.getPeripheralDeviceType());
+
+         // byte 1
+         out.writeByte(this.getPageCode());
+         
+         // byte 2 - 3
+         out.writeShort(pageLength);
+         
+         // identification descriptor list
+         for (IdentificationDescriptor id : this.descriptorList)
+         {
+            out.write(id.encode());
+         }
+
+         return baos.toByteArray();
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException("Unable to encode CDB.");
+      }
    }
 
-
-   
-
+   public void addIdentificationDescriptor(IdentificationDescriptor identificationDescriptor)
+   {
+      this.descriptorList.add(identificationDescriptor);
+   }
 }
