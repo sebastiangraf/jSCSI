036dfb0dbe9e2a609ae4e375f3bc2bcefc2be482
john_quigley <john_quigley@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-11-13 05:08:23 +0000

    completing write exercise of buffered tasks
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@353 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/test/unit/org/jscsi/scsi/tasks/buffered/BufferedWriteTaskTest.java
+++ projects/scsi/test/unit/org/jscsi/scsi/tasks/buffered/BufferedWriteTaskTest.java
@@ -1,14 +1,10 @@
 package org.jscsi.scsi.tasks.buffered;
 
 import org.apache.log4j.Logger;
-import org.jscsi.scsi.protocol.Command;
 import org.jscsi.scsi.protocol.cdb.CDB;
+import org.jscsi.scsi.protocol.cdb.Write10;
+import org.jscsi.scsi.protocol.cdb.Write12;
 import org.jscsi.scsi.protocol.cdb.Write6;
-import org.jscsi.scsi.protocol.sense.exceptions.IllegalRequestException;
-import org.jscsi.scsi.tasks.Task;
-import org.jscsi.scsi.tasks.TaskAttribute;
-import org.jscsi.scsi.transport.Nexus;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class BufferedWriteTaskTest extends BufferTestTask
@@ -24,23 +20,44 @@ public class BufferedWriteTaskTest extends BufferTestTask
    @Test
    public void simpleWrite6()
    {
+      _logger.debug("********** SIMPLE WRITE6 **********");
       CDB cdb = new Write6(false, true, 0, WRITE_BLOCKS);
-      Command cmd = new Command(new Nexus("initiator", "target", 0, 0), cdb, TaskAttribute.ORDERED, this.cmdRef, 0);
       this.createReadData(WRITE_BLOCKS * STORE_BLOCK_SIZE, cmdRef);
-      this.cmdRef++;
-
-      try
-      {
-         Task task = this.getMemoryTask(this, cmd);
-         task.run();
-         
-         task = this.getFileTask(this, cmd);
-         task.run();
-      }
-      catch (IllegalRequestException e)
-      {
-         Assert.fail("illegal request");
-      }
+      this.submitWrite(cdb, cmdRef);
       this.purgeReadData(cmdRef);
+      this.cmdRef++;
+   }
+   
+   @Test
+   public void simpleWrite10()
+   {
+      _logger.debug("********** SIMPLE WRITE10 **********");
+      CDB cdb = new Write10(0, false, false, false, false, false, 0, WRITE_BLOCKS);
+      this.createReadData(WRITE_BLOCKS * STORE_BLOCK_SIZE, cmdRef);
+      this.submitWrite(cdb, cmdRef);
+      this.purgeReadData(cmdRef);
+      this.cmdRef++;
+   }
+   
+   @Test
+   public void simpleWrite12()
+   {
+      _logger.debug("********** SIMPLE WRITE12 **********");
+      CDB cdb = new Write12(0, false, false, false, false, false, 0, WRITE_BLOCKS);
+      this.createReadData(WRITE_BLOCKS * STORE_BLOCK_SIZE, cmdRef);
+      this.submitWrite(cdb, cmdRef);
+      this.purgeReadData(cmdRef);
+      this.cmdRef++;
+   }
+   
+   @Test
+   public void simpleWrite16()
+   {
+      _logger.debug("********** SIMPLE WRITE16 **********");
+      CDB cdb = new Write12(0, false, false, false, false, false, 0, WRITE_BLOCKS);
+      this.createReadData(WRITE_BLOCKS * STORE_BLOCK_SIZE, cmdRef);
+      this.submitWrite(cdb, cmdRef);
+      this.purgeReadData(cmdRef);
+      this.cmdRef++;
    }
 }
