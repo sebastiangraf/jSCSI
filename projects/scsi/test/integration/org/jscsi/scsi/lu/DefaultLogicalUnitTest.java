5b37bea07e3989aafd7d0d526b6e6ee66fcb26f2
mmotwani <mmotwani@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-14 23:56:30 +0000

    Renamed DefaultLogicalUnit to AbstractLogicalUnit (since it was abstract)
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@387 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/test/integration/org/jscsi/scsi/lu/DefaultLogicalUnitTest.java
+++ projects/scsi/test/integration/org/jscsi/scsi/lu/DefaultLogicalUnitTest.java
@@ -20,7 +20,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class DefaultLogicalUnitTest extends DefaultLogicalUnit
+public class DefaultLogicalUnitTest extends AbstractLogicalUnit
 {
    private static Logger _logger = Logger.getLogger(DefaultLogicalUnitTest.class);
 
