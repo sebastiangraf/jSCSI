a1d837ec37011acbfaeb630558fb7a5b158d08ff
john_quigley <john_quigley@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-14 06:06:41 +0000

    adding launch files for important tests
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@375 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/test/integration/org/jscsi/scsi/lu/DefaultLogicalUnitTest.java
+++ projects/scsi/test/integration/org/jscsi/scsi/lu/DefaultLogicalUnitTest.java
@@ -2,13 +2,26 @@ package org.jscsi.scsi.lu;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
-
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class DefaultLogicalUnitTest
 {
    private static Logger _logger = Logger.getLogger(DefaultLogicalUnitTest.class);
+   
+   @BeforeClass
+   public static void setUpBeforeClass() throws Exception
+   {
+
+   }
+
+   @AfterClass
+   public static void tearDownAfterClass() throws Exception
+   {
+
+   }
    
    @Before
    public void setUp() throws Exception
