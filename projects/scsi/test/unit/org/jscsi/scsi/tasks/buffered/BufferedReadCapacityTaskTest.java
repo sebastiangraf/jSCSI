7b2260aeb8b5389522791f2530ee24d0827f74a7
jasonresch <jasonresch@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-14 23:50:29 +0000

    TK-00000
    Fixed bug where BufferedReadCapacity16Task was writing block size as a Long instead of an Int
    
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@386 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/test/unit/org/jscsi/scsi/tasks/buffered/BufferedReadCapacityTaskTest.java
+++ projects/scsi/test/unit/org/jscsi/scsi/tasks/buffered/BufferedReadCapacityTaskTest.java
@@ -1,5 +1,9 @@
 package org.jscsi.scsi.tasks.buffered;
 
+import java.nio.ByteBuffer;
+
+import static org.junit.Assert.*;
+
 import org.apache.log4j.Logger;
 import org.jscsi.scsi.protocol.cdb.CDB;
 import org.jscsi.scsi.protocol.cdb.ReadCapacity10;
@@ -20,7 +24,7 @@ public class BufferedReadCapacityTaskTest extends BufferTestTask
       _logger.debug("********** READ CAPACITY 10 **********");
       CDB cdb = new ReadCapacity10(false, 0);
       this.submitCDB(cdb, cmdRef);
-      // TODO: Verify Read Capacity Return Value
+      verifyInputBufferCapacity10();
       cmdRef++;
    }
    
@@ -28,9 +32,43 @@ public class BufferedReadCapacityTaskTest extends BufferTestTask
    public void testReadCapacity16()
    {
       _logger.debug("********** READ CAPACITY 16 **********");
-      CDB cdb = new ReadCapacity16(8, false, 0);
+      CDB cdb = new ReadCapacity16(32, false, 0);
       this.submitCDB(cdb, cmdRef);
-      // TODO: Verify Read Capacity Return Value
+      verifyInputBufferCapacity16();
       cmdRef++;
+   }
+   
+   public void verifyInputBufferCapacity10()
+   {
+      final int expectedNumberOfBlocks = STORE_CAPACITY / STORE_BLOCK_SIZE;
+      
+      ByteBuffer inputBuffer = this.getWriteData(cmdRef);
+      
+      assertNotNull(inputBuffer);
+      
+      final int returnedNumberOfBlocks = inputBuffer.getInt();
+      
+      final int returnedBlockLength = inputBuffer.getInt();
+      
+      assertEquals(expectedNumberOfBlocks, returnedNumberOfBlocks);
+      
+      assertEquals(returnedBlockLength, STORE_BLOCK_SIZE);
+   }
+   
+   public void verifyInputBufferCapacity16()
+   {
+      final long expectedNumberOfBlocks = STORE_CAPACITY / STORE_BLOCK_SIZE;
+      
+      ByteBuffer inputBuffer = this.getWriteData(cmdRef);
+      
+      assertNotNull(inputBuffer);
+      
+      final long returnedNumberOfBlocks = inputBuffer.getLong();
+      
+      final int returnedBlockLength = inputBuffer.getInt();
+      
+      assertEquals(expectedNumberOfBlocks, returnedNumberOfBlocks);
+      
+      assertEquals(returnedBlockLength, STORE_BLOCK_SIZE);
    }
 }
