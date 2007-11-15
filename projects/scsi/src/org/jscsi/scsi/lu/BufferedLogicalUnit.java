3605e63222acaf385cacb20372c7d2a4775d0c51
mmotwani <mmotwani@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-11-15 23:36:14 +0000

    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@404 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- projects/scsi/src/org/jscsi/scsi/lu/BufferedLogicalUnit.java
+++ projects/scsi/src/org/jscsi/scsi/lu/BufferedLogicalUnit.java
@@ -1,42 +1,32 @@
+
 package org.jscsi.scsi.lu;
 
 import java.nio.ByteBuffer;
 
 import org.apache.log4j.Logger;
-import org.jscsi.scsi.protocol.inquiry.InquiryDataRegistry;
 import org.jscsi.scsi.protocol.inquiry.StaticInquiryDataRegistry;
-import org.jscsi.scsi.protocol.mode.ModePageRegistry;
 import org.jscsi.scsi.protocol.mode.StaticModePageRegistry;
 import org.jscsi.scsi.tasks.buffered.BufferedTaskFactory;
 import org.jscsi.scsi.tasks.management.DefaultTaskManager;
 import org.jscsi.scsi.tasks.management.DefaultTaskSet;
-import org.jscsi.scsi.tasks.management.TaskManager;
 import org.jscsi.scsi.tasks.management.TaskSet;
 
 // TODO: Describe class or interface
 public class BufferedLogicalUnit extends AbstractLogicalUnit
-{   
+{
    private static Logger _logger = Logger.getLogger(TaskSet.class);
 
    private final int NUM_TASK_THREADS = 10;
    private final int SET_QUEUE_DEPTH = 10;
-   
-   private final TaskSet taskSet = new DefaultTaskSet(SET_QUEUE_DEPTH);
-   private final TaskManager taskManager = new DefaultTaskManager(NUM_TASK_THREADS, this.taskSet);
-   private final InquiryDataRegistry inquiryRegistry = new StaticInquiryDataRegistry();
-   private final ModePageRegistry modePageRegistry = new StaticModePageRegistry();
 
    public BufferedLogicalUnit(ByteBuffer store, int blockSize)
    {
       super();
+      TaskSet taskSet = new DefaultTaskSet(SET_QUEUE_DEPTH);
       this.setTaskSet(taskSet);
-      this.setTaskManager(taskManager);
-      
-      
-      this.setTaskFactory(new BufferedTaskFactory(
-            store,
-            blockSize,
-            this.modePageRegistry,
-            this.inquiryRegistry ));
+      this.setTaskManager(new DefaultTaskManager(NUM_TASK_THREADS, taskSet));
+
+      this.setTaskFactory(new BufferedTaskFactory(store, blockSize, new StaticModePageRegistry(),
+            new StaticInquiryDataRegistry()));
    }
 }
