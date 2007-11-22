401ad2c23f10a1fbbf0c0c351539a272b6effd62
marcus_specht <marcus_specht@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2007-11-22 14:13:32 +0000

    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@475 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

---
+++ jSCSI-target/src/org/jscsi/target/task/standard/login/LeadingLoginRequestState.java
@@ -0,0 +1,18 @@
+package org.jscsi.target.task.standard.login;
+
+import org.jscsi.target.task.abstracts.AbstractState;
+import org.jscsi.target.task.abstracts.Task;
+
+public class LeadingLoginRequestState extends AbstractState{
+
+	public LeadingLoginRequestState(Task refTask) {
+		super(refTask);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		
+		
+	}
+
+}
