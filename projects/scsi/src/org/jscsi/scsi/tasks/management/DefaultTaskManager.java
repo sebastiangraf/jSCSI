dc8e77b05a1a08701cca32c24e44189cda482ee5
john_quigley <john_quigley@fd70404c-346f-40f3-aaf5-4d485f690dad>
2007-11-30 20:30:53 +0000

    cleanups
    git-svn-id: https://svn.uni-konstanz.de/disy/jscsi/trunk@493 fd70404c-346f-40f3-aaf5-4d485f690dad

--- projects/scsi/src/org/jscsi/scsi/tasks/management/DefaultTaskManager.java
+++ projects/scsi/src/org/jscsi/scsi/tasks/management/DefaultTaskManager.java
@@ -1,3 +1,4 @@
+
 package org.jscsi.scsi.tasks.management;
 
 import java.util.concurrent.ExecutorService;
@@ -10,35 +11,33 @@ import org.jscsi.core.util.NamedThreadFactory;
 import org.jscsi.scsi.tasks.Task;
 
 public class DefaultTaskManager implements TaskManager
-{   
+{
    private static Logger _logger = Logger.getLogger(DefaultTaskManager.class);
-   
+
    private ExecutorService executor;
    private final AtomicBoolean running = new AtomicBoolean(false);
-   
+
    private TaskSet taskSet;
-   
+
    private Thread currentThread;
-   
-   
+
    /////////////////////////////////////////////////////////////////////////////
    // constructor(s)
-   
+
    public DefaultTaskManager(int numThreads, TaskSet taskSet)
    {
       executor = Executors.newFixedThreadPool(numThreads, new NamedThreadFactory("TaskExecutor"));
       this.taskSet = taskSet;
    }
-   
-   
+
    /////////////////////////////////////////////////////////////////////////////
    // TaskManager implementation
-   
+
    public void run()
    {
       this.currentThread = Thread.currentThread();
       this.running.set(true);
-      
+
       while (this.running.get())
       {
          Task nextTask = null;
@@ -60,17 +59,15 @@ public class DefaultTaskManager implements TaskManager
          }
       }
       _logger.info("Shutting down task manager.");
-      // stop processing tasks:
-      //  - clear task set
-      //  - wait for any outstanding tasks to finish executing
+
       this.taskSet.clear();
       executor.shutdown();
    }
-   
+
    public void shutdown() throws InterruptedException
    {
       this.running.set(false);
-      if ( this.currentThread != null )
+      if (this.currentThread != null)
       {
          this.currentThread.interrupt();
          this.currentThread.join();
