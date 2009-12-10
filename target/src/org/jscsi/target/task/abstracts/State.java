a59bd3a5b44d9da8d1cfc97a009b17e49d0bb25d
sebastiangraf <sebastiangraf@6391b2ca-d626-0410-90c9-8f6d1efc1dcd>
2009-12-10 12:48:53 +0000

    removed old projects
    git-svn-id: https://jscsi.svn.sourceforge.net/svnroot/jscsi/trunk@847 6391b2ca-d626-0410-90c9-8f6d1efc1dcd

--- target/src/org/jscsi/target/task/abstracts/State.java
+++
@@ -1,9 +0,0 @@
-package org.jscsi.target.task.abstracts;
-
-public interface State extends Operation{
-	
-	public Task getReferencedTask();
-	
-	public void define(MutableTask refTask) throws OperationException;
-
-}
