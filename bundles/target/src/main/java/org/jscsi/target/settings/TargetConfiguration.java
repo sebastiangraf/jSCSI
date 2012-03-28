e598964e55a8eaa81e1ac451dea0b0745a055b91
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2012-03-28 13:06:57 +0200

    OPEN - task 1: Initial setup for target http://github.com/disy/jscsi/pull/1 [MOD] fixed .gitignore (was still bound to perfidix) [MOD] switched to xs:double within xsd [MOD] Rounding within TargetConfiguration instead of casting

--- bundles/target/src/main/java/org/jscsi/target/settings/TargetConfiguration.java
+++ bundles/target/src/main/java/org/jscsi/target/settings/TargetConfiguration.java
@@ -46,41 +46,34 @@ public class TargetConfiguration {
     /**
      * The primary folder containing all configuration files.
      */
-    public static final File RELEASE_CONFIGURATION_DIRECTORY = new File(
-            new StringBuilder(System.getProperty("user.dir")).append(
-                    File.separator).toString());
+    public static final File RELEASE_CONFIGURATION_DIRECTORY = new File(new StringBuilder(System
+        .getProperty("user.dir")).append(File.separator).toString());
 
     /**
-     * The back-up folder which will be searched for the <code>xsd</code> and
-     * <code>xml</code>files if they cannot be found in the
-     * {@link #RELEASE_CONFIGURATION_DIRECTORY}.
+     * The back-up folder which will be searched for the <code>xsd</code> and <code>xml</code>files if they
+     * cannot be found in the {@link #RELEASE_CONFIGURATION_DIRECTORY}.
      */
-    public static final File DEVELOPMENT_CONFIGURATION_DIRECTORY = new File(
-            new StringBuilder(System.getProperty("user.dir"))
-                    .append(File.separator).append("src")
-                    .append(File.separator).append("main")
-                    .append(File.separator).append("resources")
-                    .append(File.separator).toString());
+    public static final File DEVELOPMENT_CONFIGURATION_DIRECTORY = new File(new StringBuilder(System
+        .getProperty("user.dir")).append(File.separator).append("src").append(File.separator).append("main")
+        .append(File.separator).append("resources").append(File.separator).toString());
 
     /**
      * The <code>xsd</code> file containing the schema information for all
      * target-specific settings.
      */
-    public static final File CONFIGURATION_FILE = getFile(
-            RELEASE_CONFIGURATION_DIRECTORY,
-            DEVELOPMENT_CONFIGURATION_DIRECTORY, CONFIGURATION_FILE_NAME);
+    public static final File CONFIGURATION_FILE = getFile(RELEASE_CONFIGURATION_DIRECTORY,
+        DEVELOPMENT_CONFIGURATION_DIRECTORY, CONFIGURATION_FILE_NAME);
 
     /**
      * The <code>xml</code> file containing all target-specific settings.
      */
-    public static final File SCHEMA_FILE = getFile(
-            RELEASE_CONFIGURATION_DIRECTORY,
-            DEVELOPMENT_CONFIGURATION_DIRECTORY, SCHEMA_FILE_NAME);
+    public static final File SCHEMA_FILE = getFile(RELEASE_CONFIGURATION_DIRECTORY,
+        DEVELOPMENT_CONFIGURATION_DIRECTORY, SCHEMA_FILE_NAME);
 
     /**
      * Searches and tries to return a {@link File} with the specified
-     * <i>fileName</i> from the given <i>mainDirectory</i>. If this {@link File}
-     * does not exist, a {@link File} with the specified <i>fileName</i> from
+     * <i>fileName</i> from the given <i>mainDirectory</i>. If this {@link File} does not exist, a
+     * {@link File} with the specified <i>fileName</i> from
      * the given <i>backUpDirectory</i> will be returned.
      * 
      * @param mainDirectory
@@ -91,8 +84,7 @@ public class TargetConfiguration {
      *            the name of the file without path information
      * @return a {@link File} that may or may not exist
      */
-    private static File getFile(final File mainDirectory,
-            final File backUpDirectory, String fileName) {
+    private static File getFile(final File mainDirectory, final File backUpDirectory, String fileName) {
         final File file = new File(mainDirectory, fileName);
         if (file.exists())
             return file;
@@ -123,8 +115,8 @@ public class TargetConfiguration {
     /**
      * The port used by the jSCSI Target for listening for new connections.
      * <p>
-     * The default port number is 3260. This value may be overridden by
-     * specifying a different value in the configuration file.
+     * The default port number is 3260. This value may be overridden by specifying a different value in the
+     * configuration file.
      */
     private int port;
 
@@ -132,23 +124,22 @@ public class TargetConfiguration {
      * The path leading the the file used as the storage medium.
      */
     private String storageFilePath;
-    
+
     /**
      * The size of the storage medium if it needs to be created.
      */
     private long storageFileLength;
 
     /**
-     * This variable toggles the strictness with which the parameters
-     * <code>IFMarkInt</code> and <code>OFMarkInt</code> are processed, when
+     * This variable toggles the strictness with which the parameters <code>IFMarkInt</code> and
+     * <code>OFMarkInt</code> are processed, when
      * provided by the initiator. Usually the offered values must have to
      * following format: <code>smallInteger~largeInteger</code>, however the
      * jSCSI Initiator sends only single integers as <i>value</i> part of the
      * <i>key-value</i> pairs. Since the value of these two parameters always
      * are <code>Irrelevant</code>, this bug can be ignored without any negative
-     * consequences by setting {@link #allowSloppyNegotiation} to
-     * <code>true</code> in the configuration file. The default is
-     * <code>false</code>.
+     * consequences by setting {@link #allowSloppyNegotiation} to <code>true</code> in the configuration file.
+     * The default is <code>false</code>.
      */
     private boolean allowSloppyNegotiation;// TODO fix in jSCSI Initiator and
                                            // remove
@@ -161,15 +152,14 @@ public class TargetConfiguration {
     /**
      * The Logical Unit Number of the virtual Logical Unit.
      */
-    private final LogicalUnitNumber logicalUnitNumber = new LogicalUnitNumber(
-            0L);
+    private final LogicalUnitNumber logicalUnitNumber = new LogicalUnitNumber(0L);
 
     /**
      * The <code>MaxRecvDataSegmentLength</code> parameter for PDUs sent in the
      * out direction (i.e. initiator to target).
      * <p>
-     * Since the value of this variable is equal to the specified default value,
-     * it does not have to be declared during login.
+     * Since the value of this variable is equal to the specified default value, it does not have to be
+     * declared during login.
      */
     private final int outMaxRecvDataSegmentLength = 8192;
 
@@ -177,17 +167,14 @@ public class TargetConfiguration {
      * The maximum number of consecutive Login PDUs or Text Negotiation PDUs the
      * target will accept in a single sequence.
      * <p>
-     * The iSCSI standard does not dictate a minimum or maximum text PDU
-     * sequence length, but only suggests to select a value large enough for all
-     * expected key-value pairs that might be sent in a single sequence. A limit
-     * should be imposed, however, to prevent {@link OutOfMemoryError}s
-     * resulting from malicious or accidental text PDU sequences of extreme
-     * lengths.
+     * The iSCSI standard does not dictate a minimum or maximum text PDU sequence length, but only suggests to
+     * select a value large enough for all expected key-value pairs that might be sent in a single sequence. A
+     * limit should be imposed, however, to prevent {@link OutOfMemoryError}s resulting from malicious or
+     * accidental text PDU sequences of extreme lengths.
      * <p>
-     * Since all common text parameters (plus values) easily fit into a single
-     * text PDU with the default data segment size, this value could be set to
-     * <code>1</code> without negatively affecting compatibility with most
-     * initiators.
+     * Since all common text parameters (plus values) easily fit into a single text PDU with the default data
+     * segment size, this value could be set to <code>1</code> without negatively affecting compatibility with
+     * most initiators.
      */
     private final int maxRecvTextPduSequenceLength = 4;
 
@@ -219,14 +206,11 @@ public class TargetConfiguration {
         return storageFilePath;
     }
 
-    /**
-	 * @return the storageFileLength
-	 */
-	public long getStorageFileLength() {
-		return storageFileLength;
-	}
+    public long getStorageFileLength() {
+        return storageFileLength;
+    }
 
-	public boolean getAllowSloppyNegotiation() {
+    public boolean getAllowSloppyNegotiation() {
         return allowSloppyNegotiation;
     }
 
@@ -251,8 +235,8 @@ public class TargetConfiguration {
      * @throws IOException
      *             If any IO errors occur.
      */
-    public static final TargetConfiguration create() throws SAXException,
-            ParserConfigurationException, IOException {
+    public static final TargetConfiguration create() throws SAXException, ParserConfigurationException,
+        IOException {
 
         return create(SCHEMA_FILE, CONFIGURATION_FILE);
     }
@@ -275,9 +259,9 @@ public class TargetConfiguration {
      * @throws IOException
      *             If any IO errors occur.
      */
-    public static final TargetConfiguration create(
-            final File configSchemaFileName, final File configFileName)
-            throws SAXException, ParserConfigurationException, IOException {
+    public static final TargetConfiguration
+        create(final File configSchemaFileName, final File configFileName) throws SAXException,
+            ParserConfigurationException, IOException {
 
         final TargetConfiguration config = new TargetConfiguration();
 
@@ -301,19 +285,16 @@ public class TargetConfiguration {
      * @throws IOException
      *             If any IO errors occur.
      */
-    private final Document parse(final File schemaLocation,
-            final File configFile) throws SAXException,
-            ParserConfigurationException, IOException {
+    private final Document parse(final File schemaLocation, final File configFile) throws SAXException,
+        ParserConfigurationException, IOException {
 
-        final SchemaFactory schemaFactory = SchemaFactory
-                .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        final SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         final Schema schema = schemaFactory.newSchema(schemaLocation);
 
         // create a validator for the document
         final Validator validator = schema.newValidator();
 
-        final DocumentBuilderFactory domFactory = DocumentBuilderFactory
-                .newInstance();
+        final DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
         domFactory.setNamespaceAware(true); // never forget this
         final DocumentBuilder builder = domFactory.newDocumentBuilder();
         final Document doc = builder.parse(configFile);
@@ -322,7 +303,7 @@ public class TargetConfiguration {
         final DOMResult result = new DOMResult();
 
         validator.validate(source, result);
-        return (Document) result.getNode();
+        return (Document)result.getNode();
     }
 
     /**
@@ -333,41 +314,37 @@ public class TargetConfiguration {
      */
     private final void parseSettings(final Element root) {
         // TargetName
-        targetName = root.getElementsByTagName(TextKeyword.TARGET_NAME).item(0)
-                .getTextContent();
+        targetName = root.getElementsByTagName(TextKeyword.TARGET_NAME).item(0).getTextContent();
 
         // TargetAlias (optional)
-        final Node targetAliasNode = root.getElementsByTagName(
-                TextKeyword.TARGET_ALIAS).item(0);
+        final Node targetAliasNode = root.getElementsByTagName(TextKeyword.TARGET_ALIAS).item(0);
         if (targetAliasNode != null)
             targetAlias = targetAliasNode.getTextContent();
         // else it is null
 
         // port
         if (root.getElementsByTagName("Port").getLength() > 0)
-            port = Integer.parseInt(root.getElementsByTagName("Port").item(0)
-                    .getTextContent());
+            port = Integer.parseInt(root.getElementsByTagName("Port").item(0).getTextContent());
         else
             port = 3260;
-        
+
         final NodeList fileProperties = root.getElementsByTagName("StorageFile").item(0).getChildNodes();
         for (int i = 0; i < fileProperties.getLength(); ++i) {
-            if ("FilePath".equals(fileProperties.item(i).getNodeName())){
+            if ("FilePath".equals(fileProperties.item(i).getNodeName())) {
                 storageFilePath = fileProperties.item(i).getTextContent();
-            } else if("FileLength".equals(fileProperties.item(i).getNodeName())){
-            	this.storageFileLength = (long)((Float.valueOf(fileProperties.item(i).getTextContent())) *Math.pow(1024, 3));
+            } else if ("FileLength".equals(fileProperties.item(i).getNodeName())) {
+                this.storageFileLength =
+                    Math.round(((Double.valueOf(fileProperties.item(i).getTextContent())) * Math.pow(1024, 3)));
             }
         }
         if (storageFilePath == null)
             storageFilePath = "storage.dat";
 
         // support sloppy text parameter negotiation (i.e. the jSCSI Initiator)?
-        final Node allowSloppyNegotiationNode = root.getElementsByTagName(
-                "AllowSloppyNegotiation").item(0);
+        final Node allowSloppyNegotiationNode = root.getElementsByTagName("AllowSloppyNegotiation").item(0);
         if (allowSloppyNegotiationNode == null)
             allowSloppyNegotiation = false;
         else
-            allowSloppyNegotiation = Boolean
-                    .parseBoolean(allowSloppyNegotiationNode.getTextContent());
+            allowSloppyNegotiation = Boolean.parseBoolean(allowSloppyNegotiationNode.getTextContent());
     }
 }
