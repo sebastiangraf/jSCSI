82b0c424262014ae952a35de54ca88b68e4a70b7
saenridanra <andreas.rain@gmail.com>
2013-11-14 15:01:32 +0100

    [MOD] Had to modify the activator in order for the bundle to work.

--- bundles/target/src/main/java/org/jscsi/target/Activator.java
+++ bundles/target/src/main/java/org/jscsi/target/Activator.java
@@ -1,6 +1,11 @@
 package org.jscsi.target;
 
+import java.io.File;
+import java.io.InputStream;
 import java.net.InetAddress;
+import java.nio.file.Files;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -15,6 +20,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator implements BundleActivator {
 
+    private ExecutorService runner;
     private TargetServer target;
     
     @Override
@@ -22,21 +28,39 @@ public class Activator implements BundleActivator {
         // The osgi container or system properties have to supply
         // a property 'jscsi_target-published_ip', to determine
         // which ip address should be used.
+        System.out.println("Starting up jscsi bundle");
         String pIp = context.getProperty("jscsi_target-published_ip");
         InetAddress addr = InetAddress.getByName(pIp);
 
         if (addr == null)
             throw new IllegalArgumentException(
                 "Either the address provided with 'jscsi_target-published_ip' is illegal or not contained within the available network interfaces.");
+      
+        File schemaFile = new File("jscsi-target.xsd");
+        
+        if(schemaFile.exists() == false){
+            Files.copy(context.getBundle().getResource("/jscsi-target.xsd").openStream(), schemaFile.toPath());
+        }
+        
+        System.out.println("Schemafile: " + ((schemaFile.exists())? "exists" : "does not exist"));
+        
+        File configFile = new File("jscsi-target.xml");
+        if(configFile.exists() == false){
+            Files.copy(context.getBundle().getResource("/jscsi-target.xml").openStream(), configFile.toPath());
+        }
+        
+        System.out.println("Configfile: " + ((configFile.exists())? "exists" : "does not exist"));
 
-        target = new TargetServer(Configuration.create(addr.getHostAddress()));
+        target = new TargetServer(Configuration.create(schemaFile, configFile, addr.getHostAddress()));
 
-        target.call();
+        runner = Executors.newSingleThreadExecutor();
+        runner.submit(target);
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         // Need to provide a shutdown method within the jscsi target
+        runner.shutdown();
         System.exit(-1);
     }
 
