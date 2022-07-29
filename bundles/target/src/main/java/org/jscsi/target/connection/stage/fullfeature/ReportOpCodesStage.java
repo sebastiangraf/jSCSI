ff76938e4deb3e3511c7131edc97105de4a973f4
ActiveVolcano <chenqc@gmail.com>
2022-07-29 11:35:20 +0800

    Support PB level storage by adding READ (16), READ (12), WRITE (16), WRITE (12), REPORT SUPPORTED OPERATION CODES commands

---
+++ bundles/target/src/main/java/org/jscsi/target/connection/stage/fullfeature/ReportOpCodesStage.java
@@ -0,0 +1,57 @@
+package org.jscsi.target.connection.stage.fullfeature;
+
+
+import java.io.IOException;
+import java.security.DigestException;
+
+import org.jscsi.exception.InternetSCSIException;
+import org.jscsi.parser.BasicHeaderSegment;
+import org.jscsi.parser.ProtocolDataUnit;
+import org.jscsi.parser.scsi.SCSICommandParser;
+import org.jscsi.target.connection.phase.TargetFullFeaturePhase;
+import org.jscsi.target.scsi.cdb.OpCodesCDB;
+import org.jscsi.target.scsi.report.OneOpCode;
+import org.jscsi.target.settings.SettingsException;
+import org.jscsi.target.util.Debug;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A stage for processing <code>REPORT SUPPORTED OPERATION CODES</code> SCSI commands.
+ *
+ * @author CHEN Qingcan
+ */
+public class ReportOpCodesStage extends TargetFullFeatureStage {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReportOpCodesStage.class);
+
+    public ReportOpCodesStage (TargetFullFeaturePhase targetFullFeaturePhase) {
+        super(targetFullFeaturePhase);
+    }
+
+    @Override
+    public void execute (ProtocolDataUnit pdu) throws IOException , InterruptedException , InternetSCSIException , DigestException , SettingsException {
+        final BasicHeaderSegment bhs = pdu.getBasicHeaderSegment();
+        final SCSICommandParser parser = (SCSICommandParser) bhs.getParser();
+
+        // get command details in CDB
+        if (LOGGER.isDebugEnabled()) {// print CDB bytes
+            LOGGER.debug("CDB bytes: \n" + Debug.byteBufferToString(parser.getCDB()));
+        }
+        final OpCodesCDB cdb = new OpCodesCDB(parser.getCDB());
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("cdb.getAllocationLength() = " + cdb.getAllocationLength());
+            LOGGER.debug("cdb.isNormalACA() = " + cdb.isNormalACA());
+            LOGGER.debug("cdb.getReportingOptions() = " + cdb.getReportingOptions());
+        }
+
+        // send response
+        sendResponse (
+            bhs.getInitiatorTaskTag(),
+            parser.getExpectedDataTransferLength(),
+            new OneOpCode (cdb.getReportingOptions())
+        );
+    }
+
+}
